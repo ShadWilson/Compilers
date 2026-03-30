f594e9c4e6e2cc246f2b661aa2511bdfd264f840
Shad Wilson <sawilson27@wabash.edu>
2026-03-30 19:47:42 -0400

    fixed pa7

--- pa7/program1.asm
+++ pa7/program1.asm
@@ -2,35 +2,39 @@
 ;It should read the value of both variables from the user (using GETI), and then using branch instructions, print precisely one of the strings based on whether 
 ;a<b, a=b, or a>b.  
 
-.data
-msg_lt: .asciiz "a is less than b\n"
-msg_eq: .asciiz "a is equal to b\n"
-msg_gt: .asciiz "a is greater than b\n"
-
 .text
 .globl main
 main:
 
-    ;a
-    GETI t0
-    ;b
-    GETI t1
+;a
+GETI t0
+;b
+GETI t1
 
-    # compare
-    blt t0, t1, less
-    beq t0, t1, equal
-    j greater
+# compare
+BLT t0, t1, less
+BEQ t0, t1, equal
+J greater
 
 less:
-    PUTS msg_lt
-    j end
+LA t0, 0x10000000
+PUTS t0
+J end
 
 equal:
-    PUTS msg_eq
-    j end
+LA t0, 0x10000004
+PUTS t0
+J end
 
 greater:
-    PUTS msg_gt
+LA t0, 0x10000008
+PUTS t0
+J end
 
 end:
-    HALT
+HALT
+
+.section .strings
+0x10000000 "a is less than b\n"
+0x10000004 "a is equal to b\n"
+0x10000008 "a is greater than b\n"
--- pa7/program2.asm
+++ pa7/program2.asm
@@ -3,57 +3,59 @@
 ;Once the user has given a valid value of n, it should do the Collatz function in a while loop, printing n each time before changing n, 
 ;and also adding 1 to the count.  After the loop exits, it should print n (which will now be 1) and then also print the count.\
 
-.data
-prompt:  .asciiz "Enter a positive integer n: "
-newline: .asciiz "\n"
-
 .text
 .globl main
 main:
-
-    li t0, 0
-    li t1, 0
+LI t0, 0
+LI t1, 0
 
 input_loop:
-    PUTS prompt
-    GETI t0
-    blez t0, input_loop  
+LA t0, 0x10000000
+PUTS t0
+GETI t0
+BLEZ t0, input_loop  
 
 collatz_loop:
+PUTI t0
+LA t0, 0x10000004
+PUTS t0
 
-    PUTI t0
-    PUTS newline
+LI t2, 1
+BEQ t0, t2, done
 
-    li t2, 1
-    beq t0, t2, done
+ADDI t1, t1, 1
 
-    addi t1, t1, 1
+LI t2, 2
+DIV t3, t0, t2
+MUL t3, t3, t2
+SUB t4, t0, t3 
 
-    li t2, 2
-    div t3, t0, t2
-    mul t3, t3, t2
-    sub t4, t0, t3 
-
-    beqz t4, even_case
+BEQZ t4, even_case
 
 odd_case:
-    li t2, 3
-    mul t0, t0, t2
-    addi t0, t0, 1
-    j collatz_loop
+LI t2, 3
+MUL t0, t0, t2
+ADDI t0, t0, 1
+J collatz_loop
 
 even_case:
-    li t2, 2
-    div t0, t0, t2
-    j collatz_loop
+LI t2, 2
+DIV t0, t0, t2
+J collatz_loop
 
 done:
-    # print final n (1)
-    PUTI t0
-    PUTS newline
+# print final n (1)
+PUTI t0
+LA t0, 0x10000004
+PUTS t0
 
-    # print count
-    PUTI t1
-    PUTS newline
+# print count
+PUTI t1
+LA t0, 0x10000004
+PUTS t0
 
-    HALT
+HALT
+
+.section .strings
+0x10000000 "Enter a positive integer n: "
+0x10000004 "\n"
